79bb0763050ae242840cef1446d78b389a1340bb
cocoytech <i.am.cocoytech+github@gmail.com>
2019-08-16 00:51:02 +0800

    Add contactinformation use case by implementing its interface

---
+++ source/Microservice00000.Contacts.Domain/Services/ContactInformationUseCase.cs
@@ -0,0 +1,18 @@
+﻿using Microservice00000.Contacts.Domain.Contracts;
+using Microservice00000.Contacts.Domain.Entities;
+using Microservice00000.Contacts.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microservice00000.Contacts.Domain.Services
+{
+    public class ContactInformationUseCase : IContactInformationUseCase
+    {
+        public ContactResult CreateContact(Int64 exepctId, string expectFirstName, string expectLastName, string expectedContactNumber)
+        {
+            Contact contactInformation = new Contact(exepctId, expectFirstName, expectLastName, expectedContactNumber);
+            return new ContactResult(contactInformation);
+        }
+    }
+}
