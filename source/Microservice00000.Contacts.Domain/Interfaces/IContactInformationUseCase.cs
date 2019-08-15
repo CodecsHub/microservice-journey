f967d92bc7cd17cdf55947f17d33ad6d4491538d
cocoytech <i.am.cocoytech+github@gmail.com>
2019-08-16 00:48:15 +0800

    Add create contact in contact information use case to implement the test case

---
+++ source/Microservice00000.Contacts.Domain/Interfaces/IContactInformationUseCase.cs
@@ -0,0 +1,13 @@
+﻿using Microservice00000.Contacts.Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microservice00000.Contacts.Domain.Interfaces
+{
+    public interface IContactInformationUseCase
+    {
+        ContactResult CreateContact(Int64 exepctId, string expectFirstName, string expectLastName, string expectedContactNumber);
+
+    }
+}
