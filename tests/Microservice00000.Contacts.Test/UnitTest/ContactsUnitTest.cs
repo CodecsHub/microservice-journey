7d303751787631152003b6850201593295b57b12
cocoytech <i.am.cocoytech+github@gmail.com>
2019-08-16 00:15:33 +0800

    Start create Tests class for contact unit testing

---
+++ tests/Microservice00000.Contacts.Test/UnitTest/ContactsUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Microservice00000.Contacts.Test.UnitTest
+{
+    /// <summary>
+    /// The class for unit testing in Contacts Entities
+    /// </summary>
+    public class ContactsUnitTest
+    {
+        [Fact]
+        public void CreateSingleContactInformation__Returns_SingleContactInformation()
+        {
+            //
+            // Arrange
+            string expectName = "John Doe";
+            string expectedPhoneNumber = "+76 1 400 180 66";
+            //ICreateContactInformation createContactInformation = new CreateContactInformation();
+
+            // Act
+            //CustomerResult actualCustomerResult = registerUC.Register(expectName, expectedPhoneNumber);
+
+            // Assert
+            //Assert.NotNull(actualCustomerResult);
+            //Assert.NotEqual(Guid.Empty, actualCustomerResult.Id);
+            //Assert.Equal(expectName, actualCustomerResult.Name);
+            //Assert.Equal(expectedPhoneNumber, actualCustomerResult.PhoneNumber);
+        }
+
+    }
+}
