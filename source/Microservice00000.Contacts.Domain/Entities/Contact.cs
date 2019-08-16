4c1d447f021dd29060a34680d36c3c67b7d1c1b2
cocoytech <i.am.cocoytech+github@gmail.com>
2019-08-16 23:20:25 +0800

    Start clean code commenting on the contact entity

--- source/Microservice00000.Contacts.Domain/Entities/Contact.cs
+++ source/Microservice00000.Contacts.Domain/Entities/Contact.cs
@@ -5,44 +5,70 @@ using System.Text;
 namespace Microservice00000.Contacts.Domain.Entities
 {
     /// <summary>
-    /// Sealed class to be avoided used outside from the domain excep instantion
-    /// in infrastructure, presenter and application
+    /// The sealed Contact class.
+    /// The entity for contact information
     /// </summary>
+    /// <remarks>
+    /// <para>This class is consist of properties are private set but get in open via constructor</para>
+    /// <para>These operations can be performed on contact information combination of string and number</para>
+    /// </remarks>
     public sealed class Contact
     {
         /// <summary>
         /// The unique Id for the contact information entity.
         /// </summary>
+        /// <value>Gets the value of Id and private set from contructor</value>
         public Int64 Id { get; private set;}
 
         /// <summary>
         /// The first name of the contact information.
         /// </summary>
+        /// <value>Gets the value of FirstName and private set from contructor</value>
         public string FirstName { get; private set; }
 
         /// <summary>
         /// The last Name of the contact information.
         /// </summary>
+        /// <value>Gets the value of LastName and private set from contructor</value>
         public string LastName { get; private set; }
 
         /// <summary>
         /// The contact number of the contact information.
         /// </summary>
+        /// <value>Gets the value of ContactNumber and private set from contructor</value>
         public string ContactNumber { get; private set; }
 
         /// <summary>
         /// Entry point of the contact information system
         /// </summary>
+        /// <return>
+        /// get the return data set in the constructor
+        /// </return>
+        /// <example>
+        /// <code>
+        /// Contact contactInformation = new Contact(exepctId, expectFirstName, expectLastName, expectedContactNumber);
+        /// return contactInformation;
+        /// </code>
+        /// </example>
         /// <param name="id">831</param>
         /// <param name="firstname">Francisco</param>
         /// <param name="lastname">Abayon</param>
         /// <param name="contactNumber">092248678349</param>
         public Contact(Int64 id, string firstname, string lastname, string contactNumber)
         {
-            this.Id = id;
-            this.FirstName = firstname;
-            this.LastName = lastname;
-            this.ContactNumber = contactNumber;
+
+            try
+            {
+                this.Id = id;
+                this.FirstName = firstname;
+                this.LastName = lastname;
+                this.ContactNumber = contactNumber;
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
 
         }
     }
--- tests/Microservice00000.Contacts.Test/UnitTest/ContactsUnitTest.cs
+++ tests/Microservice00000.Contacts.Test/UnitTest/ContactsUnitTest.cs
@@ -5,7 +5,9 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
-
+/// <summary>
+/// The unit test of contact class
+/// </summary>
 namespace Microservice00000.Contacts.Test.UnitTest
 {
     /// <summary>
@@ -16,7 +18,8 @@ namespace Microservice00000.Contacts.Test.UnitTest
         [Fact]
         public void CreateSingleContactInformation__Returns_SingleContactInformation()
         {
-            //
+
+            // ToDo: refactor to single mock unit class
             // Arrange
             Int64 exepctId = 1;
             string expectFirstName = "Francisco";
@@ -31,6 +34,7 @@ namespace Microservice00000.Contacts.Test.UnitTest
             // Assert
             Assert.NotNull(actualContactResult);
             Assert.NotEqual(0, actualContactResult.Id);
+            Assert.Equal(expectFirstName, actualContactResult.FirstName);
             Assert.Equal(expectFirstName, actualContactResult.FirstName);
             Assert.Equal(expectLastName, actualContactResult.LastName);
             Assert.Equal(expectedContactNumber, actualContactResult.ContactNumber);
